5c3065c9345368993cb06a15e4ebf9ef3f55bc6e
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-10-28 09:40:12 +0100

    feat: new ssh key

--- base/remote_access.nix
+++ base/remote_access.nix
@@ -9,5 +9,6 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUFV67ZGk8kIdPvho4IBF4fLMemDNVPbhNlnaVcatIP"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPw8RDATFPx9rcMqtT7h3tDCPYOyz0fnN5OLQ+3KJxe3"
   ];
 }
